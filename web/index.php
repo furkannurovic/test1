6958bf9447877e1447f3bf8fea08e9103e890527
Ishak Dervisevic <ishak.dervisevic@protonmail.com>
2020-07-23 22:59:44 +0200

    fdsa

--- web/index.php
+++ web/index.php
@@ -13,15 +13,15 @@ $adress = $_POST["userMessage"];
 $mail = new PHPMailer;
 
 $mail->isSMTP();    
-$mail->Host = ' smtp.zoho.eu';                 // Specify main and backup SMTP servers
+$mail->Host = 'smtp.zoho.eu';                 // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;        
 $mail->Port = 587;      
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;                           // Enable encryption, 'ssl' also accepted                  // Enable SMTP authentication
-$mail->Username = 'email@ishakdervisevic.com';                 // SMTP username
-$mail->Password = 'atwas64atwwbwb32';                           // SMTP password
+$mail->Username = 'furkan.nurovic@zohomail.eu';                 // SMTP username
+$mail->Password = 'Furkan123456789';                           // SMTP password
 
-$mail->From = 'email@ishakdervisevic.com';
-$mail->FromName = 'Ishak Dervisevic';
+$mail->From = 'furkan.nurovic@zohomail.eu';
+$mail->FromName = 'Furkan Nurovic';
 $mail->addAddress('ekrem.nurovic@ibu.edu.ba', 'Ekrem Nurovic');     // Add a recipient
 
 $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
